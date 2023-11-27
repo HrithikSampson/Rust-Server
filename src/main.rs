151d643cebb21ae5d75e037f7cb136ecde455052
Hrithik Sampson <hrithikedwardsampson@gmail.com>
2023-11-28 01:33:00 +0530

    pass 2nd stage

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,27 @@
+use std::io::BufRead;
+use std::io::Write;
 use std::net::TcpListener;
-
+use std::io::prelude;
+use std::io::BufReader;
+use std::net::TcpStream;
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
     // listener is a iterator to a array of streams
     for stream in listener.incoming(){
         let stream = stream.unwrap();// overshadowing stream
-        // stream is a connection attempt and when os limit reaches and throws error
+        
         println!("Connection established");
+        // stream is a connection attempt and when os limit reaches and throws error
+        handle_connection(stream);
     }
+}
+fn handle_connection(mut stream:TcpStream){
+    let http_request: Vec<_> = BufReader::new(&mut stream)
+                                       .lines()
+                                       .map(|el|el.unwrap())
+                                       .take_while(|el| !el.is_empty())
+                                       .collect();
+    println!("{:#?}",http_request);
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    stream.write_all(response.as_bytes()).unwrap();
 }
