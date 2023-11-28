3ef3fc80634d7e3045f573f2f94a385ecf062bba
Hrithik Sampson <hrithikedwardsampson@gmail.com>
2023-11-29 02:31:53 +0530

    pass 8th stage

--- src/main.rs
+++ src/main.rs
@@ -99,7 +99,7 @@ fn handle_connection(mut stream:TcpStream,directory: Option<String>){
             //     }, 
             //     Err(_) => {
 
-    let mut file = File::create(&filepath).unwrap();
+    let mut file = File::create(&filepath).unwrap_or_else(|err| panic!("error: {}",err));
     file.write_all(contents.as_bytes()).unwrap();
 
     let response = "HTTP/1.1 201 Created\r\n\r\n";
