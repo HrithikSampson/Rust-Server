d9c17f1f6e8e4d36a421093d48213f13bbe3b671
Hrithik Sampson <hrithikedwardsampson@gmail.com>
2023-11-29 12:36:30 +0530

    pass 8th stage

--- src/main.rs
+++ src/main.rs
@@ -43,7 +43,7 @@ fn handle_connection(mut stream:TcpStream,directory: Option<String>){
                                        .collect();
     println!("{:#?}",http_request); 
     
-    let mut content_buffer = vec![0u8;1024];
+    let mut content_buffer = vec![];
     let _sz=stream.read(&mut content_buffer);
     
     println!("{:?}",content_buffer);
