7733b5b95d7fa9839636d54532b08b993ba775c1
Hrithik Sampson <hrithikedwardsampson@gmail.com>
2023-11-29 11:44:30 +0530

    pass 8th stage

--- src/main.rs
+++ src/main.rs
@@ -44,7 +44,7 @@ fn handle_connection(mut stream:TcpStream,directory: Option<String>){
     println!("{:#?}",http_request); 
     
     let mut content_buffer = [0u8; 1024];
-    let _sz=stream.read(&mut content_buffer).unwrap();
+    let _sz=stream.read(&mut content_buffer);
     let iter = http_request.get(0).unwrap().split(' ').map(|el|el.to_string());
     let bind = iter.collect::<Vec<String>>();
     let path = bind.get(1).unwrap();
